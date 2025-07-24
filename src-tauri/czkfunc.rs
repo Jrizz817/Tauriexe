ab910404f79e8176a99b472c3954d18f4f476457
Jrizz817 <jrclientsof@gmail.com>
2025-07-24 14:15:15 -0300

    Update czkfunc.rs

--- src-tauri/czkfunc.rs
+++ src-tauri/czkfunc.rs
@@ -1,5 +1,6 @@
 use libloading::{Library, Symbol};
 use std::sync::OnceLock;
+use std::path::PathBuf;
 
 static LIB: OnceLock<Library> = OnceLock::new();
 
@@ -8,14 +9,20 @@ pub struct CzkFUNC;
 impl CzkFUNC {
     fn get_lib() -> &'static Library {
         LIB.get_or_init(|| {
-            unsafe { Library::new("src-tauri/libs/CzkAPI.dll").expect("Falha ao carregar DLL CzkAPI.dll") }
+            let path: PathBuf = std::env::current_exe()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .join("src-tauri/libs/CzkAPI.dll");
+            unsafe { Library::new(path).expect("Falha ao carregar a DLL") }
         })
     }
 
     pub fn execute(script: &str) {
         unsafe {
             let lib = Self::get_lib();
-            let func: Symbol<unsafe extern "C" fn(*const u8, usize)> = lib.get(b"Execute").expect("Função Execute não encontrada na DLL");
+            let func: Symbol<unsafe extern "C" fn(*const u8, usize)> =
+                lib.get(b"execute").expect("Função 'execute' não encontrada");
             func(script.as_ptr(), script.len());
         }
     }
@@ -23,8 +30,9 @@ impl CzkFUNC {
     pub fn inject() {
         unsafe {
             let lib = Self::get_lib();
-            let inject_fn: Symbol<unsafe extern "C" fn()> = lib.get(b"Inject").expect("Função Inject não encontrada na DLL");
-            inject_fn();
+            let func: Symbol<unsafe extern "C" fn()> =
+                lib.get(b"inject").expect("Função 'inject' não encontrada");
+            func();
         }
     }
 }
